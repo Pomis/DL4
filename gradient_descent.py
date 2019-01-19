e1b4e96bb4bdd4eeb965e6a7246c232fe3029903
Roman Ismagilov <pomis172@gmail.com>
2019-01-19 20:36:20 +0100

    first experiment

---
+++ gradient_descent.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import tensorflow as tf
+import sys
+import matplotlib.pyplot as plt
+
+# Model parameters
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+# Model input and output
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+y = tf.placeholder(tf.float32)
+
+# loss
+loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+
+steps = [0.005, 0.001, 0.01, 0.02]
+for step in steps:
+    # optimizer
+    optimizer = tf.train.GradientDescentOptimizer(step)
+    train = optimizer.minimize(loss)
+
+    # training data
+    x_train = [1, 2, 3, 4]
+    y_train = [0, -1, -2, -3]
+    # training loop
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init) # reset values to wrong
+
+    curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+    print("%s,%s"%(0, curr_loss))
+
+    x_list = [0]
+    y_list = [curr_loss]
+
+    for i in range(1000):
+        sess.run(train, {x: x_train, y: y_train})
+        curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+        print("%s,%s"%(i, curr_loss))
+        x_list.append(i)
+        y_list.append(curr_loss)
+
+    plt.plot(x_list,y_list,label=step)
+    plt.legend()
+
+plt.ylabel('loss')
+plt.xlabel('iteration')
+plt.show()
+
+
+
